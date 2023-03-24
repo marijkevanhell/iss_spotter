66f6d5eb2f9ebb392b08d07f46184b98657d2cc4
marijkevanhell <jukevanhell@gmail.com>
2023-03-24 10:00:43 -0700

    ISS Spotter II

--- index.js
+++ index.js
@@ -8,3 +8,15 @@
 
 //   console.log('It worked! Returned IP:' , ip);
 // });
+
+
+// const { fetchCoordsByIP } = require('./iss');
+
+// fetchCoordsByIP('108.172.237.70', (error, coordinates) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned coordinates:' , coordinates);
+// });
--- iss.js
+++ iss.js
@@ -15,4 +15,21 @@ const fetchMyIP = function(callback) {
   });
 };
 
+
+const fetchCoordsByIP = function(ip, callback) {
+  request(`http://ipwho.is/${ip}`, (error, response, body) => {
+    if (error) return callback(error, null);
+
+    const parsedBody = JSON.parse(body);
+    if (!parsedBody.success) {
+      const message = `Success status was ${parsedBody.success}. Server message says: ${parsedBody.message} when fetching for IP ${parsedBody.ip}`;
+      callback(Error(message), null);
+      return;
+    }
+    const { latitude, longitude } = parsedBody;
+    callback(null, {latitude, longitude});
+  });
+};
+
 module.exports = { fetchMyIP };
+module.exports = { fetchCoordsByIP };
